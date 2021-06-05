7453b7ed907ab95f690ad65b0bf18c348507414e
Zavala <toro.d.c@gmail.com>
2021-06-05 14:06:07 -0400

    js used to format table and sightings data added to table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Set variable for table body reference
+var tbody = d3.select("tbody");
+
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
